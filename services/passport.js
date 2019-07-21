77cea5b4b12aea555c22ce3f2fa94be29f448fc5
alexisjcarr <acarr59@gmail.com>
2019-07-21 10:33:01 -0500

    🚀 Adds proxy true option to Google strategy.
     #1

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleId: profile.id });
