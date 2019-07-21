8715de817c3ce5d694745f0e05441b36cd76fb4d
alexisjcarr <acarr59@gmail.com>
2019-07-20 19:52:16 -0500

    ➕ Adds mongoose and configures MongoDB db.
     #1

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 require("./services/passport");
-
 const authRouter = require("./routers/authRouter");
+
+mongoose.connect(process.env.MONGO_URI);
 
 const server = express();
 
