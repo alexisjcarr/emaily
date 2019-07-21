985c968a661436c17cbf1b7ea0bd572fb5cd93bd
alexisjcarr <acarr59@gmail.com>
2019-07-21 09:49:57 -0500

    ➕ Adds welcome message on base API page.
     #1

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require("./server");
 
 const PORT = process.env.PORT || 5000;
 
+server.get("/", (req, res) => {
+  res.send("Welcome to base camp.");
+});
+
 server.listen(PORT, () => {
   console.log(`\n:::I'm working on port ${PORT}:::\n`);
 });
