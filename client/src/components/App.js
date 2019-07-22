1a5f75001892b0b7a4ed2e2c7899767787cb9ce1
alexisjcarr <acarr59@gmail.com>
2019-07-21 21:46:17 -0500

    ➕ Adds redux-thunk and axios.
     #1

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,7 +8,7 @@ const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Switch>
         <Route exact path="/" component={Landing} />
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import materializeCSS from "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
