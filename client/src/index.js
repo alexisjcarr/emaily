92a09df20452f254aed26a37493449d59328c544
alexisjcarr <acarr59@gmail.com>
2019-07-21 21:32:06 -0500

    ✨ Adds materialiCSS import to index.js
     #1

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import materializeCSS from "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
